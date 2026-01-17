6f8b74fcc33aed9ddc107f2bd492bedb5265b979
Raggamuffin <ivehlzr@gmail.com>
2026-01-17 15:05:50 +0300

    Initial project setup from spring.io

---
+++ src/main/java/com/borda/borda_service/BordaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.borda.borda_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BordaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BordaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/borda/borda_service/BordaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.borda.borda_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BordaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
